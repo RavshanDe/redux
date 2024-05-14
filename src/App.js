6eb07c8f4fc21a1d25d48325bdcc6ecb3f0fe63c
Ravshan <javascriptphyton@gmail.com>
2024-05-14 19:19:47 +0500

    changed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-medium mt-2 py-4">
+        12 -Redux store
+      </h1>
+      ;
+    </div>
+  );
+}
+
+export default App;
